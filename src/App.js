3807b0c90bcfdf933e5a8efc40a46e7f2d7ce086
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-10 03:30:36 -0500

    complete birdcard.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import birds from "./data/birds"
 import BirdCard from "./Components/BirdCard.js";
+import "./App.css"
 
 function App () {
   return (
     <div>
-      <h1>Bird Sanctuary</h1>
-      {birds.map((bird) => (
-        <BirdCard bird={bird} key={bird.id} />
-      ))}
+      <header className="style__title">
+        <h1> Bird Sanctuary Project</h1>
+        <cite> By Vandhana Mohan</cite>
+      </header>
+      
+      <div className="card">
+        {birds.map((bird) => (
+          <BirdCard bird={bird} key={bird.id} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/Components/BirdCard.js
+++ src/Components/BirdCard.js
@@ -1,16 +1,17 @@
+import "./BirdCard.css"
+
 const BirdCard = ({ bird }) => {
     const { img, name, amount } = bird
     return (
         <main>
-            <h2>{name}</h2>
-            <h4>Price : {amount} </h4>
+            <h4>{name}</h4>
+            <p>Price: ${amount} </p>
             <span>
                 <img src={img} alt="bird" />
             </span>
-            <button>Adopt</button>
+            <br></br>
+            <button>ADOPT</button>
         </main>
     )
 }
 export default BirdCard;
-  
-  

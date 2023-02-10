59863305c14f231c416d88f759b2e388998d6b4c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-10 04:53:03 -0500

    Edit index.html link element to add icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,25 +1,23 @@
 import "./Footer.css"
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous"></link>
 
 const Footer = () => {
     return(
         <footer>
             <nav>
                 <ul class="social_links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Social Media</a></li>
-                        <a href="#"><span class="twitter"></span></a>
-                        <a href="#"><span class="instagram"></span></a>
-                        <a href="#"><span class="linkedin"></span></a>
-                        <a href="#"><span class="facebook"></span></a>
-                        <a href="#"><span class="github"></span></a>
+                    <a class="footer__facebook" href="https://facebook.com/" target="_blank"><i class="fab fa-facebook-square" alt="Facebook"></i></a>
+                    <a class="footer__instagram" href="https://instagram.com/" target="_blank"><i class="fab fa-instagram" alt="Instagram"></i></a>
+                    <a class="footer__twitter" href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square" alt="Twitter"></i></a>
+                    <a class="footer__youtube" href="https://youtube.com/" target="_blank"><i class="fab fa-youtube" alt="YouTube"></i></a>
+                    <a class="footer__github" href="https://github.com/Vandhana-Mohan" target="_blank"><i class="fab fa-github" alt="GitHub"></i></a>
+                    <a class="footer__github" href="https://www.linkedin.com/in/vandhanamohan/" target="_blank"><i class="fab fa-linkedin" alt="linkedin"></i></a>
+
                 </ul>
             </nav>
-            <p>Copyright &copy; 2023 Vandhana Mohan</p>
+            <p>Copyright © 2023 <a href="https://github.com/Vandhana-Mohan" target="_blank">Vandhana Mohan.</a> All rights reserved.</p>
         </footer>
     )
 }
 export default Footer
-
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,8 +5,26 @@ const Header = () => {
         <header className="style__title">
             <h1> Bird Sanctuary Project</h1>
             <cite> By Vandhana Mohan</cite>
+            <nav>
+                <ul>
+                    <li>
+                        <button>Home</button>
+                    </li>
+                    <li>
+                        <button>About Us</button>
+                    </li>
+                    <li>
+                        <button>Contact Us</button>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
-
 export default Header
+
+
+
+
+            
+       

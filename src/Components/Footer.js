6e2155bc1afb2543a8347db0ef569d499d71062e
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-10 06:43:07 -0500

    add navbar.css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,42 +2,42 @@ import "./Footer.css"
 
 const Footer = () => {
     return(
-        <footer>
+        <footer className="style__footer">
             <nav>
-                <ul className="social_links">
+                <ul className="style__footer__social__links">
                     <li>
-                        <a className="footer__facebook" href="https://facebook.com/" target="_blank" rel="noopener noreferrer">
+                        <a className="style__footer__facebook" href="https://facebook.com/" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-facebook-square" alt="Facebook"></i>
                         </a>
                     </li>
                     <li>
-                        <a className="footer__instagram" href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
+                        <a className="style__footer__instagram" href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-instagram" alt="Instagram"></i>
                         </a>
                     </li>
                     <li>
-                        <a className="footer__twitter" href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                        <a className="style__footer__twitter" href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-twitter-square" alt="Twitter"></i>
                         </a>
                     </li>
                     <li>
-                        <a className="footer__youtube" href="https://youtube.com/" target="_blank" rel="noopener noreferrer">
+                        <a className="style__footer__youtube" href="https://youtube.com/" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-youtube" alt="YouTube"></i>
                         </a>
                     </li>
                     <li>
-                        <a className="footer__github" href="https://github.com/Vandhana-Mohan" target="_blank" rel="noopener noreferrer">
+                        <a className="style__footer__github" href="https://github.com/Vandhana-Mohan" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-github" alt="GitHub"></i>
                         </a>
                     </li>
                     <li>
-                        <a className="footer__linkedin" href="https://www.linkedin.com/in/vandhanamohan/" target="_blank" rel="noopener noreferrer">
+                        <a className="style__footer__linkedin" href="https://www.linkedin.com/in/vandhanamohan/" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-linkedin" alt="LinkedIn"></i>
                         </a>
                     </li>
                 </ul>
             </nav>
-            <p>Copyright © 2023 <a href="https://github.com/Vandhana-Mohan" target="_blank" rel="noopener noreferrer">Vandhana Mohan</a>. All rights reserved.</p>
+            <p className="style__copyright">Copyright © 2023 <a href="https://github.com/Vandhana-Mohan" target="_blank" rel="noopener noreferrer">Vandhana Mohan</a>. All rights reserved.</p>
         </footer>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,7 @@ const Header = () => {
     return(
         <header className="style__header">
             <nav className="style__header__nav">
-                <h1> Bird Sanctuary Project</h1>
-                <cite> By Vandhana Mohan</cite>
+                <h1 className="style__header__title"> B i r d - S a n c t u a r y</h1>
                 <ul className="style__header__ul">
                     <li className="style__header__li">
                         <button className="style__header__buttons">Home</button>
